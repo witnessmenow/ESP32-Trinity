a9cbad9e19fe84aecefc0433a5cd1a4430aa43be
witnessmenow <ecenuig@gmail.com>
2020-12-03 01:31:59 +0000

    Create WifiTetrisClock.ino

---
+++ examples/WifiTetrisClock/WifiTetrisClock.ino
@@ -0,0 +1,313 @@
+/*******************************************************************
+    Tetris clock using a 64x32 RGB Matrix that fetches its
+    time over WiFi using the EzTimeLibrary.
+
+    For use with my I2S Matrix Shield.
+
+    Parts Used:
+    ESP32 D1 Mini * - https://s.click.aliexpress.com/e/_dSi824B
+    ESP32 I2S Matrix Shield (From my Tindie) = https://www.tindie.com/products/brianlough/esp32-i2s-matrix-shield/
+
+      = Affilate
+
+    If you find what I do useful and would like to support me,
+    please consider becoming a sponsor on Github
+    https://github.com/sponsors/witnessmenow/
+
+    Written by Brian Lough
+    YouTube: https://www.youtube.com/brianlough
+    Tindie: https://www.tindie.com/stores/brianlough/
+    Twitter: https://twitter.com/witnessmenow
+ *******************************************************************/
+
+// ----------------------------
+// Standard Libraries - Already Installed if you have ESP32 set up
+// ----------------------------
+
+#include <WiFi.h>
+
+// ----------------------------
+// Additional Libraries - each one of these will need to be installed.
+// ----------------------------
+
+#include <ESP32-RGB64x32MatrixPanel-I2S-DMA.h>
+// This is the library for interfacing with the display
+
+// Can be installed from the library manager (Search for "ESP32 64x32 LED MATRIX")
+// https://github.com/mrfaptastic/ESP32-RGB64x32MatrixPanel-I2S-DMA
+
+// Adafruit GFX library is a dependancy for the matrix Library
+// Can be installed from the library manager
+// https://github.com/adafruit/Adafruit-GFX-Library
+
+#include <TetrisMatrixDraw.h>
+// This library draws out characters using a tetris block
+// amimation
+// Can be installed from the library manager
+// https://github.com/toblum/TetrisAnimation
+
+#include <ezTime.h>
+// Library used for getting the time and adjusting for DST
+// Search for "ezTime" in the Arduino Library manager
+// https://github.com/ropg/ezTime
+
+// ---- Stuff to configure ----
+
+// Initialize Wifi connection to the router
+char ssid[] = "SSID";     // your network SSID (name)
+char password[] = "password"; // your network key
+
+// Set a timezone using the following list
+// https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+#define MYTIMEZONE "Europe/Dublin"
+
+// Sets whether the clock should be 12 hour format or not.
+bool twelveHourFormat = true;
+
+// If this is set to false, the number will only change if the value behind it changes
+// e.g. the digit representing the least significant minute will be replaced every minute,
+// but the most significant number will only be replaced every 10 minutes.
+// When true, all digits will be replaced every minute.
+bool forceRefresh = true;
+// -----------------------------
+
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+hw_timer_t * animationTimer = NULL;
+
+unsigned long animationDue = 0;
+unsigned long animationDelay = 100; // Smaller number == faster
+
+RGB64x32MatrixPanel_I2S_DMA dma_display(true); //true enables the double buffer
+
+uint16_t myBLACK = dma_display.color565(0, 0, 0);
+
+TetrisMatrixDraw tetris(dma_display); // Main clock
+TetrisMatrixDraw tetris2(dma_display); // The "M" of AM/PM
+TetrisMatrixDraw tetris3(dma_display); // The "P" or "A" of AM/PM
+
+Timezone myTZ;
+unsigned long oneSecondLoopDue = 0;
+
+bool showColon = true;
+volatile bool finishedAnimating = false;
+bool displayIntro = true;
+
+String lastDisplayedTime = "";
+String lastDisplayedAmPm = "";
+
+
+// This method is for controlling the tetris library draw calls
+void animationHandler()
+{ 
+  // Not clearing the display and redrawing it when you
+  // dont need to improves how the refresh rate appears
+  if (!finishedAnimating) {
+    dma_display.flipDMABuffer();
+    dma_display.fillScreen(myBLACK);
+    if (displayIntro) {
+      finishedAnimating = tetris.drawText(1, 21);
+    } else {
+      if (twelveHourFormat) {
+        // Place holders for checking are any of the tetris objects
+        // currently still animating.
+        bool tetris1Done = false;
+        bool tetris2Done = false;
+        bool tetris3Done = false;
+
+        tetris1Done = tetris.drawNumbers(-6, 26, showColon);
+        tetris2Done = tetris2.drawText(56, 25);
+
+        // Only draw the top letter once the bottom letter is finished.
+        if (tetris2Done) {
+          tetris3Done = tetris3.drawText(56, 15);
+        }
+
+        finishedAnimating = tetris1Done && tetris2Done && tetris3Done;
+
+      } else {
+        finishedAnimating = tetris.drawNumbers(2, 26, showColon);
+      }
+    }
+    dma_display.showDMABuffer();
+  }
+
+}
+
+void drawIntro(int x = 0, int y = 0)
+{
+  dma_display.flipDMABuffer();
+  dma_display.fillScreen(myBLACK);
+  tetris.drawChar("P", x, y, tetris.tetrisCYAN);
+  tetris.drawChar("o", x + 5, y, tetris.tetrisMAGENTA);
+  tetris.drawChar("w", x + 11, y, tetris.tetrisYELLOW);
+  tetris.drawChar("e", x + 17, y, tetris.tetrisGREEN);
+  tetris.drawChar("r", x + 22, y, tetris.tetrisBLUE);
+  tetris.drawChar("e", x + 27, y, tetris.tetrisRED);
+  tetris.drawChar("d", x + 32, y, tetris.tetrisWHITE);
+  tetris.drawChar(" ", x + 37, y, tetris.tetrisMAGENTA);
+  tetris.drawChar("b", x + 42, y, tetris.tetrisYELLOW);
+  tetris.drawChar("y", x + 47, y, tetris.tetrisGREEN);
+  dma_display.showDMABuffer();
+}
+
+void drawConnecting(int x = 0, int y = 0)
+{
+  dma_display.flipDMABuffer();
+  dma_display.fillScreen(myBLACK);
+  tetris.drawChar("C", x, y, tetris.tetrisCYAN);
+  tetris.drawChar("o", x + 5, y, tetris.tetrisMAGENTA);
+  tetris.drawChar("n", x + 11, y, tetris.tetrisYELLOW);
+  tetris.drawChar("n", x + 17, y, tetris.tetrisGREEN);
+  tetris.drawChar("e", x + 22, y, tetris.tetrisBLUE);
+  tetris.drawChar("c", x + 27, y, tetris.tetrisRED);
+  tetris.drawChar("t", x + 32, y, tetris.tetrisWHITE);
+  tetris.drawChar("i", x + 37, y, tetris.tetrisMAGENTA);
+  tetris.drawChar("n", x + 42, y, tetris.tetrisYELLOW);
+  tetris.drawChar("g", x + 47, y, tetris.tetrisGREEN);
+  dma_display.showDMABuffer();
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  // Attempt to connect to Wifi network:
+  Serial.print("Connecting Wifi: ");
+  Serial.println(ssid);
+
+  // Set WiFi to station mode and disconnect from an AP if it was Previously
+  // connected
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  // Do not set up display before WiFi connection
+  // as it will crash!
+
+  // Intialise display library
+  dma_display.begin();
+  dma_display.fillScreen(myBLACK);
+
+  // "connecting"
+  drawConnecting(5, 10);
+
+  // Setup EZ Time
+  setDebug(INFO);
+  waitForSync();
+
+  Serial.println();
+  Serial.println("UTC:             " + UTC.dateTime());
+
+  myTZ.setLocation(F(MYTIMEZONE));
+  Serial.print(F("Time in your set timezone:         "));
+  Serial.println(myTZ.dateTime());
+
+  // "Powered By"
+  drawIntro(6, 12);
+  delay(2000);
+
+  // Start the Animation Timer
+  tetris.setText("I2S SHIELD");
+
+  // Wait for the animation to finish
+  while (!finishedAnimating)
+  {
+    delay(animationDelay);
+    animationHandler();
+  }
+  delay(2000);
+  finishedAnimating = false;
+  displayIntro = false;
+  tetris.scale = 2;
+  //tetris.setTime("00:00", true);
+}
+
+void setMatrixTime() {
+  String timeString = "";
+  String AmPmString = "";
+  if (twelveHourFormat) {
+    // Get the time in format "1:15" or 11:15 (12 hour, no leading 0)
+    // Check the EZTime Github page for info on
+    // time formatting
+    timeString = myTZ.dateTime("g:i");
+
+    //If the length is only 4, pad it with
+    // a space at the beginning
+    if (timeString.length() == 4) {
+      timeString = " " + timeString;
+    }
+
+    //Get if its "AM" or "PM"
+    AmPmString = myTZ.dateTime("A");
+    if (lastDisplayedAmPm != AmPmString) {
+      Serial.println(AmPmString);
+      lastDisplayedAmPm = AmPmString;
+      // Second character is always "M"
+      // so need to parse it out
+      tetris2.setText("M", forceRefresh);
+
+      // Parse out first letter of String
+      tetris3.setText(AmPmString.substring(0, 1), forceRefresh);
+    }
+  } else {
+    // Get time in format "01:15" or "22:15"(24 hour with leading 0)
+    timeString = myTZ.dateTime("H:i");
+  }
+
+  // Only update Time if its different
+  if (lastDisplayedTime != timeString) {
+    Serial.println(timeString);
+    lastDisplayedTime = timeString;
+    tetris.setTime(timeString, forceRefresh);
+
+    // Must set this to false so animation knows
+    // to start again
+    finishedAnimating = false;
+  }
+}
+
+void handleColonAfterAnimation() {
+
+  // It will draw the colon every time, but when the colour is black it
+  // should look like its clearing it.
+  uint16_t colour =  showColon ? tetris.tetrisWHITE : tetris.tetrisBLACK;
+  // The x position that you draw the tetris animation object
+  int x = twelveHourFormat ? -6 : 2;
+  // The y position adjusted for where the blocks will fall from
+  // (this could be better!)
+  int y = 26 - (TETRIS_Y_DROP_DEFAULT * tetris.scale);
+  tetris.drawColon(x, y, colour);
+  dma_display.showDMABuffer();
+}
+
+
+void loop() {
+  unsigned long now = millis();
+  if (now > oneSecondLoopDue) {
+    // We can call this often, but it will only
+    // update when it needs to
+    setMatrixTime();
+    showColon = !showColon;
+
+    // To reduce flicker on the screen we stop clearing the screen
+    // when the animation is finished, but we still need the colon to
+    // to blink
+    if (finishedAnimating) {
+      handleColonAfterAnimation();
+    }
+    oneSecondLoopDue = now + 1000;
+  }
+  now = millis();
+  if(now > animationDue) {
+    animationHandler();
+    animationDue = now + animationDelay;
+  }
+}

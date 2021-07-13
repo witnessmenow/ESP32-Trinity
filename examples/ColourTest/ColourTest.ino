1ce9780676a441419256e77198811bdaf28c3d9b
witnessmenow <ecenuig@gmail.com>
2021-07-13 23:43:10 +0100

    Update ColourTest.ino

--- examples/ColourTest/ColourTest.ino
+++ examples/ColourTest/ColourTest.ino
@@ -1,32 +1,97 @@
-#include <ESP32-RGB64x32MatrixPanel-I2S-DMA.h>
+/*******************************************************************
+    A simple sketch that just cycles through colours
 
-RGB64x32MatrixPanel_I2S_DMA dma_display;
+    NOTE: !!! Careful! This sketch can use a lot of power
+    so make sure your power supply is up to the task
 
-uint16_t myBLACK = dma_display.color565(0, 0, 0);
-uint16_t myWHITE = dma_display.color565(255, 255, 255);
-uint16_t myRED = dma_display.color565(255, 0, 0);
-uint16_t myGREEN = dma_display.color565(0, 255, 0);
-uint16_t myBLUE = dma_display.color565(0, 0, 255);
+    For use with my I2S Matrix Shield.
+
+    Parts Used:
+    ESP32 D1 Mini * - https://s.click.aliexpress.com/e/_dSi824B
+    ESP32 I2S Matrix Shield (From my Tindie) = https://www.tindie.com/products/brianlough/esp32-i2s-matrix-shield/
+
+      = Affiliate
+
+    If you find what I do useful and would like to support me,
+    please consider becoming a sponsor on Github
+    https://github.com/sponsors/witnessmenow/
+
+    Written by Brian Lough
+    YouTube: https://www.youtube.com/brianlough
+    Tindie: https://www.tindie.com/stores/brianlough/
+    Twitter: https://twitter.com/witnessmenow
+ *******************************************************************/
+
+// ----------------------------
+// Additional Libraries - each one of these will need to be installed.
+// ----------------------------
+
+#include <ESP32-HUB75-MatrixPanel-I2S-DMA.h>
+// This is the library for interfacing with the display
+
+// Can be installed from the library manager (Search for "ESP32 MATRIX DMA")
+// https://github.com/mrfaptastic/ESP32-HUB75-MatrixPanel-I2S-DMA
+
+// ----------------------------
+// Dependency Libraries - each one of these will need to be installed.
+// ----------------------------
+
+// Adafruit GFX library is a dependency for the matrix Library
+// Can be installed from the library manager
+// https://github.com/adafruit/Adafruit-GFX-Library
+
+
+// -------------------------------------
+// -------   Matrix Config   ------
+// -------------------------------------
+
+#define PANEL_RES_X 64      // Number of pixels wide of each INDIVIDUAL panel module. 
+#define PANEL_RES_Y 32     // Number of pixels tall of each INDIVIDUAL panel module.
+#define PANEL_CHAIN 1      // Total number of panels chained one to another
+
+MatrixPanel_I2S_DMA *dma_display = nullptr;
+
+uint16_t myBLACK = dma_display->color565(0, 0, 0);
+uint16_t myWHITE = dma_display->color565(255, 255, 255);
+uint16_t myRED = dma_display->color565(255, 0, 0);
+uint16_t myGREEN = dma_display->color565(0, 255, 0);
+uint16_t myBLUE = dma_display->color565(0, 0, 255);
 
 void setup() {
-  // put your setup code here, to run once:
-  dma_display.begin();
-  dma_display.setPanelBrightness(MATRIX_WIDTH);
-  dma_display.clearScreen();
-  dma_display.fillScreen(myWHITE);
-  
+  HUB75_I2S_CFG mxconfig(
+    PANEL_RES_X,   // module width
+    PANEL_RES_Y,   // module height
+    PANEL_CHAIN    // Chain length
+  );
+
+  mxconfig.gpio.e = 18;
+
+  // May or may not be needed depending on your matrix
+  // Example of what needing it looks like:
+  // https://github.com/mrfaptastic/ESP32-HUB75-MatrixPanel-I2S-DMA/issues/134#issuecomment-866367216
+  //mxconfig.clkphase = false;
+
+  //mxconfig.driver = HUB75_I2S_CFG::FM6126A;
+
+  // Display Setup
+  dma_display = new MatrixPanel_I2S_DMA(mxconfig);
+  dma_display->begin();
+  dma_display->setBrightness8(100); //0-255
+  dma_display->clearScreen();
+  dma_display->fillScreen(myWHITE);
+
 }
 
 void loop() {
   delay(5000);
-  dma_display.fillScreen(myBLACK);
+  dma_display->fillScreen(myBLACK);
   delay(5000);
-  dma_display.fillScreen(myBLUE);
+  dma_display->fillScreen(myBLUE);
   delay(5000);
-  dma_display.fillScreen(myRED);
+  dma_display->fillScreen(myRED);
   delay(5000);
-  dma_display.fillScreen(myGREEN);
+  dma_display->fillScreen(myGREEN);
   delay(5000);
-  dma_display.fillScreen(myWHITE);
+  dma_display->fillScreen(myWHITE);
 
 }

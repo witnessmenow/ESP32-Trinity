005918b83337bf18c4beb245133daa13112015f4
witnessmenow <ecenuig@gmail.com>
2021-08-27 08:57:18 +0100

    Update HelloWorld.ino

--- examples/Text/HelloWorld/HelloWorld.ino
+++ examples/Text/HelloWorld/HelloWorld.ino
@@ -1,39 +1,87 @@
 // An example showing different size fonts in different colours
 
 
-#include <ESP32-HUB75-MatrixPanel-I2S-DMA.h>
-MatrixPanel_I2S_DMA dma_display;
+// ----------------------------
+// Additional Libraries - each one of these will need to be installed.
+// ----------------------------
 
-uint16_t myBLACK = dma_display.color565(0, 0, 0);
-uint16_t myWHITE = dma_display.color565(255, 255, 255);
-uint16_t myRED = dma_display.color565(255, 0, 0);
-uint16_t myGREEN = dma_display.color565(0, 255, 0);
-uint16_t myBLUE = dma_display.color565(0, 0, 255);
+#include <ESP32-HUB75-MatrixPanel-I2S-DMA.h>
+// This is the library for interfacing with the display
+
+// Can be installed from the library manager (Search for "ESP32 MATRIX DMA")
+// https://github.com/mrfaptastic/ESP32-HUB75-MatrixPanel-I2S-DMA
+
+
+// ----------------------------
+// Dependency Libraries - each one of these will need to be installed.
+// ----------------------------
+
+// Adafruit GFX library is a dependency for the matrix Library
+// Can be installed from the library manager
+// https://github.com/adafruit/Adafruit-GFX-Library
+
+// -------------------------------------
+// -------   Matrix Config   ------
+// -------------------------------------
+
+#define PANEL_RES_X 64      // Number of pixels wide of each INDIVIDUAL panel module. 
+#define PANEL_RES_Y 64     // Number of pixels tall of each INDIVIDUAL panel module.
+#define PANEL_CHAIN 1      // Total number of panels chained one to another
+
+//------------------------------------------------------------------------------------------------------------------
+
+MatrixPanel_I2S_DMA *dma_display = nullptr;
+
+uint16_t myBLACK = dma_display->color565(0, 0, 0);
+uint16_t myWHITE = dma_display->color565(255, 255, 255);
+uint16_t myRED = dma_display->color565(255, 0, 0);
+uint16_t myGREEN = dma_display->color565(0, 255, 0);
+uint16_t myBLUE = dma_display->color565(0, 0, 255);
+
 
 void setup() {
 
   Serial.begin(115200);
+  HUB75_I2S_CFG mxconfig(
+    PANEL_RES_X,   // module width
+    PANEL_RES_Y,   // module height
+    PANEL_CHAIN    // Chain length
+  );
 
-  dma_display.begin();
+  //mxconfig.double_buff = true;
+  mxconfig.gpio.e = 18;
 
-  dma_display.clearScreen();
-  dma_display.fillScreen(myBLACK);
-  dma_display.setTextWrap(false);
+
+  // May or may not be needed depending on your matrix
+  // Example of what needing it looks like:
+  // https://github.com/mrfaptastic/ESP32-HUB75-MatrixPanel-I2S-DMA/issues/134#issuecomment-866367216
+  //mxconfig.clkphase = false;
   
-  dma_display.setTextSize(1);     // size 1 == 8 pixels high
-  dma_display.setTextColor(myBLUE);
-  dma_display.setCursor(0, 0);
-  dma_display.print("Hello");
+  //mxconfig.driver = HUB75_I2S_CFG::FM6126A;
 
-  dma_display.setTextSize(2);     // size 2 == 16 pixels high
-  dma_display.setTextColor(myGREEN);
-  dma_display.setCursor(0, 8);
-  dma_display.print("Hello");
+  // Display Setup
+  dma_display = new MatrixPanel_I2S_DMA(mxconfig);
+  dma_display->begin();
 
-  dma_display.setTextSize(3);     // size 3 == 24 pixels high
-  dma_display.setTextColor(myRED);
-  dma_display.setCursor(0, 24);
-  dma_display.print("Hello");
+
+  dma_display->clearScreen();
+  dma_display->fillScreen(myBLACK);
+  dma_display->setTextWrap(false);
+  
+  dma_display->setTextSize(1);     // size 1 == 8 pixels high
+  dma_display->setTextColor(myBLUE);
+  dma_display->setCursor(0, 0);
+  dma_display->print("Hello");
+
+  dma_display->setTextSize(2);     // size 2 == 16 pixels high
+  dma_display->setTextColor(myGREEN);
+  dma_display->setCursor(0, 8);
+  dma_display->print("Hello");
+
+  dma_display->setTextSize(3);     // size 3 == 24 pixels high
+  dma_display->setTextColor(myRED);
+  dma_display->setCursor(0, 24);
+  dma_display->print("Hello");
 
 }
 
